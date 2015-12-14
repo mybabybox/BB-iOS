819f511ea0ff43b3eaaec704ec17f977ec482ff5
Mac <Mac@Macs-MacBook-Pro.local>
2015-12-14 23:30:21 +0530

    navigation bar changes

--- babybox/babybox/Constants.swift
+++ babybox/babybox/Constants.swift
@@ -14,4 +14,11 @@ struct constants {
     static let imagesBaseURL = "http://192.168.1.152:9005";
     static let currencySymbol = "$";
     static var accessToken = "";
+    static var userInfo: UserInfoVM? = nil
+    
+    
+    //Color 
+    //Pink Button color - 0xFF76A4 // RGB - 255 118 164
+    //Navigation bar color - 0xFF4C8A // RGB - 255 76 138
+    
 }
