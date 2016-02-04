cd9af1dd4d9e3b4d951aa9f709b077fbadb4a937
Anshul <anshulgupta@Anshuls-MacBook-Pro.local>
2016-02-04 21:39:38 +0530

    changes to sign up

--- babybox/babybox/SignupViewController.swift
+++ babybox/babybox/SignupViewController.swift
@@ -17,13 +17,50 @@ class SignupViewController: UIViewController {
     
     @IBOutlet weak var passwordText: UITextField!
     
-    @IBOutlet weak var confirmPasswordText: UITextField!
+       @IBOutlet weak var confirmPasswordText: UITextField!
     
+   
+    @IBOutlet var signUp: UIButton!
     override func viewDidAppear(animated: Bool) {
         self.navigationController?.navigationBar.hidden = false
     }
+   
     override func viewDidLoad() {
         self.navigationController?.navigationBar.hidden = false
+        
+        
+        let color = BabyboxUtils.babyBoxUtils.UIColorFromRGB(0xFF76A4).CGColor
+        self.firstNameText.backgroundColor = UIColor.clearColor()
+        self.firstNameText.layer.cornerRadius = 3
+        self.firstNameText.layer.borderWidth = 1
+        self.firstNameText.layer.borderColor = color
+        self.lastNameText.backgroundColor = UIColor.clearColor()
+        self.lastNameText.layer.cornerRadius = 3
+        self.lastNameText.layer.borderWidth = 1
+        self.lastNameText.layer.borderColor = color
+        self.passwordText.backgroundColor = UIColor.clearColor()
+        self.passwordText.layer.cornerRadius = 3
+        self.passwordText.layer.borderWidth = 1
+        self.passwordText.layer.borderColor = color
+        self.emailText.backgroundColor = UIColor.clearColor()
+        self.emailText.layer.cornerRadius = 3
+        self.emailText.layer.borderWidth = 1
+        self.emailText.layer.borderColor = color
+        self.confirmPasswordText.backgroundColor = UIColor.clearColor()
+        self.confirmPasswordText.layer.cornerRadius = 3
+        self.confirmPasswordText.layer.borderWidth = 1
+        self.confirmPasswordText.layer.borderColor = color
+        self.signUp.layer.cornerRadius = 3
+        self.signUp.layer.borderWidth = 1
+        self.signUp.layer.borderColor = color
+        
+       /* let cb = Checkbox(frame: CGRect(x: 20, y: 100, width: 50, height: 50))
+        cb.borderColor = UIColor.redColor()
+        cb.borderWidth = 3
+        cb.checkColor = UIColor.redColor()
+        cb.checkWidth = 3
+        view.addSubview(cb)*/
+
     }
     
     override func viewDidDisappear(animated: Bool) {
