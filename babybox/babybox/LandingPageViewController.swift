e3a32b0e5c750ef198728a2eba9cadc88990e5de
Mac <Mac@Macs-MacBook-Pro-2.local>
2016-02-15 21:02:01 +0530

    login screen UI

--- babybox/babybox/LandingPageViewController.swift
+++ babybox/babybox/LandingPageViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class LandingPageViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class LandingPageViewController: UIViewController {
 
+    @IBOutlet weak var uiContainerView: UIView!
     @IBOutlet weak var signUpBtn: UIButton!
     @IBOutlet weak var loginBtn: UIButton!
-    @IBOutlet weak var scrollVew: UIScrollView!
-    @IBOutlet var pageControl: UIPageControl!
+    //@IBOutlet weak var scrollVew: UIScrollView!
+    //@IBOutlet var pageControl: UIPageControl!
     
     override func viewDidAppear(animated: Bool) {
         self.navigationController?.toolbar.hidden = true
@@ -25,36 +26,24 @@ class LandingPageViewController: UIViewController, UIScrollViewDelegate, UIImage
         
         self.navigationController?.interactivePopGestureRecognizer?.enabled = false
         
-        self.scrollVew.pagingEnabled = true
-        self.scrollVew.frame=CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
-        let scrollViewWidth:CGFloat = self.scrollVew.frame.width
-        let scrollViewHeight:CGFloat = self.scrollVew.frame.height
+        let storyboard = UIStoryboard(name: "CarouselStoryboard", bundle: nil)
+        let vc : MVEmbeddedCarouselViewController = storyboard.instantiateInitialViewController() as! MVEmbeddedCarouselViewController
         
-        let tutorialImg_1 = UIImageView(frame:CGRectMake(0, 0, scrollViewWidth, scrollViewHeight))
-        tutorialImg_1.image=UIImage(named:"Welcome_1")
-        self.scrollVew.addSubview(tutorialImg_1)
+        vc.imageLoader = imageViewLoadFromPath
+        vc.imagePaths = [
+            "welcome_1",
+            "welcome_2",
+            "welcome_3"
+        ]
+        // Then, add to view hierarchy
+        vc.addAsChildViewController(self, attachToView:self.uiContainerView)
         
-        let tutorialImg_2 = UIImageView(frame:CGRectMake(scrollViewWidth, 0, scrollViewWidth, scrollViewHeight))
-        tutorialImg_2.image=UIImage(named:"Welcome_2")
-        self.scrollVew.addSubview(tutorialImg_2)
-        
-        let tutorialImg_3 = UIImageView(frame:CGRectMake(scrollViewWidth*2, 0, scrollViewWidth, scrollViewHeight))
-        tutorialImg_3.image=UIImage(named:"Welcome_3")
-        self.scrollVew.addSubview(tutorialImg_3)
-        
-        self.scrollVew.contentSize = CGSizeMake(self.scrollVew.frame.width * 6, self.scrollVew.frame.height)
-        self.scrollVew.delegate = self
-        self.pageControl.currentPage = 0
         
         let color = ImageUtil.imageUtil.UIColorFromRGB(0xFF76A4).CGColor
         self.signUpBtn.backgroundColor = UIColor.clearColor()
-        //self.signUpBtn.layer.cornerRadius = 5
-        //self.signUpBtn.layer.borderWidth = 1
         ImageUtil.displayButtonRoundBorder(self.signUpBtn)
         self.signUpBtn.layer.borderColor = color
         
-        //self.loginBtn.layer.cornerRadius = 5
-        //self.loginBtn.layer.borderWidth = 1
         ImageUtil.displayButtonRoundBorder(self.loginBtn)
         self.loginBtn.layer.borderColor = color
     }
@@ -72,28 +61,20 @@ class LandingPageViewController: UIViewController, UIScrollViewDelegate, UIImage
         return true
     }
     
-    //MARK: UIScrollViewDelegate
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        print("inside scroll view .. ")
-        // Test the offset and calculate the current page after scrolling ends
-        let pageWidth:CGFloat = CGRectGetWidth(scrollView.frame)
-        var currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
-        print("current age")
-        print(currentPage)
-        if (currentPage > 2.0) {
-            print("matched")
-            currentPage = 0.0
-        }
-        // Change the indicator
-        self.pageControl.currentPage = Int(currentPage)
-        // Change the text accordingly
-        if Int(currentPage) == 0{
-            //textView.text = "Sweettutos.com is your blog of choice for Mobile tutorials"
-        }else if Int(currentPage) == 1{
-            //textView.text = "I write mobile tutorials mainly targeting iOS"
-        }else if Int(currentPage) == 2{
-        }
+    var imageViewLoadFromPath: ((imageView: UIImageView, imagePath : String, completion: (newImage: Bool) -> ()) -> ()) = {
+        (imageView: UIImageView, imagePath : String, completion: (newImage: Bool) -> ()) in
+        
+        var url = NSURL(string: imagePath)
+        var image = UIImage(named: imagePath)
+        imageView.image = image
+        //imageView.kf_setImageWithURL(url!)
+        
     }
     
-    
+    var imageViewLoadCached : ((imageView: UIImageView, imagePath : String, completion: (newImage: Bool) -> ()) -> ()) = {
+        (imageView: UIImageView, imagePath : String, completion: (newImage: Bool) -> ()) in
+        
+        imageView.image = UIImage(named:imagePath)
+        completion(newImage: imageView.image != nil)
+    }
 }
