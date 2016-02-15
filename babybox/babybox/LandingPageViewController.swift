258e5194f2fd365d2c11d568b240f1bc6d84fa99
Mac <Mac@Macs-MacBook-Pro-2.local>
2016-02-15 17:14:39 +0530

    login page fix

--- babybox/babybox/LandingPageViewController.swift
+++ babybox/babybox/LandingPageViewController.swift
@@ -8,32 +8,57 @@
 
 import UIKit
 
-class LandingPageViewController: UIViewController, UINavigationControllerDelegate {
+class LandingPageViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var signUpBtn: UIButton!
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var scrollVew: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
     
     override func viewDidAppear(animated: Bool) {
         self.navigationController?.toolbar.hidden = true
         self.navigationController?.navigationBar.hidden = true
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.interactivePopGestureRecognizer?.enabled = false
-        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.scrollVew.pagingEnabled = true
+        self.scrollVew.frame=CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
+        let scrollViewWidth:CGFloat = self.scrollVew.frame.width
+        let scrollViewHeight:CGFloat = self.scrollVew.frame.height
+        
+        let tutorialImg_1 = UIImageView(frame:CGRectMake(0, 0, scrollViewWidth, scrollViewHeight))
+        tutorialImg_1.image=UIImage(named:"Welcome_1")
+        self.scrollVew.addSubview(tutorialImg_1)
+        
+        let tutorialImg_2 = UIImageView(frame:CGRectMake(scrollViewWidth, 0, scrollViewWidth, scrollViewHeight))
+        tutorialImg_2.image=UIImage(named:"Welcome_2")
+        self.scrollVew.addSubview(tutorialImg_2)
+        
+        let tutorialImg_3 = UIImageView(frame:CGRectMake(scrollViewWidth*2, 0, scrollViewWidth, scrollViewHeight))
+        tutorialImg_3.image=UIImage(named:"Welcome_3")
+        self.scrollVew.addSubview(tutorialImg_3)
+        
+        self.scrollVew.contentSize = CGSizeMake(self.scrollVew.frame.width * 6, self.scrollVew.frame.height)
+        self.scrollVew.delegate = self
+        self.pageControl.currentPage = 0
         
         let color = ImageUtil.imageUtil.UIColorFromRGB(0xFF76A4).CGColor
         self.signUpBtn.backgroundColor = UIColor.clearColor()
+        //self.signUpBtn.layer.cornerRadius = 5
+        //self.signUpBtn.layer.borderWidth = 1
         ImageUtil.displayButtonRoundBorder(self.signUpBtn)
         self.signUpBtn.layer.borderColor = color
         
+        //self.loginBtn.layer.cornerRadius = 5
+        //self.loginBtn.layer.borderWidth = 1
         ImageUtil.displayButtonRoundBorder(self.loginBtn)
         self.loginBtn.layer.borderColor = color
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -45,6 +70,29 @@ class LandingPageViewController: UIViewController, UINavigationControllerDelegat
             self.navigationController?.navigationBar.hidden = false
         }
         return true
+    }
+    
+    //MARK: UIScrollViewDelegate
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        print("inside scroll view .. ")
+        // Test the offset and calculate the current page after scrolling ends
+        let pageWidth:CGFloat = CGRectGetWidth(scrollView.frame)
+        var currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
+        print("current age")
+        print(currentPage)
+        if (currentPage > 2.0) {
+            print("matched")
+            currentPage = 0.0
+        }
+        // Change the indicator
+        self.pageControl.currentPage = Int(currentPage)
+        // Change the text accordingly
+        if Int(currentPage) == 0{
+            //textView.text = "Sweettutos.com is your blog of choice for Mobile tutorials"
+        }else if Int(currentPage) == 1{
+            //textView.text = "I write mobile tutorials mainly targeting iOS"
+        }else if Int(currentPage) == 2{
+        }
     }
     
     
