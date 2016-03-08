73f3011a643cfada14bf2036dd7f4a99cca69793
Mac <Mac@Macs-MacBook-Pro-2.local>
2016-03-09 01:10:45 +0530

    chat changes

--- babybox/babybox/ChatBubble.swift
+++ babybox/babybox/ChatBubble.swift
@@ -6,7 +6,7 @@ class ChatBubble: UIView {
     var imageViewBG: UIImageView?
     var text: String?
     var labelChatText: UILabel?
-    
+    var timeAgoLbl: UILabel?
     /**
     Initializes a chat bubble view
     
@@ -24,7 +24,6 @@ class ChatBubble: UIView {
         self.backgroundColor = UIColor.clearColor()
         
         let padding: CGFloat = 10.0
-        
         // 2. Drawing image if any
         if let chatImage = data.image {
             
@@ -36,29 +35,59 @@ class ChatBubble: UIView {
             imageViewChat?.layer.masksToBounds = true
             self.addSubview(imageViewChat!)
         } else if (data.imgId != -1 && data.imgId != nil) {
-            //let width: CGFloat = min(chatImage.size.width, CGRectGetWidth(self.frame) - 2 * padding)
-            //let height: CGFloat = chatImage.size.height * (width / chatImage.size.width)
-            imageViewChat = UIImageView(frame: CGRectMake(5, 5, 45, 45))
-            
+            imageViewChat = UIImageView(frame: CGRectMake(-10, 5, 30, 30))
             ImageUtil.displayThumbnailProfileImage(data.imgId!, imageView: imageViewChat!)
             self.addSubview(imageViewChat!)
         }
         
+        //Create Child SubView for showing opponet details
+        let subView = UIView(frame: CGRectMake(0, 0, 25, 25))
+        subView.backgroundColor = UIColor.grayColor()
+        subView.layer.backgroundColor = ImageUtil.UIColorFromRGB(0xffffff).CGColor
+        self.addSubview(subView)
+        
         // 3. Going to add Text if any
-        if let chatText = data.text {
+        if let _ = data.text {
             // frame calculation
-            let startX = padding
-            var startY:CGFloat = 5.0
-            if let imageView = imageViewChat {
-                startY += CGRectGetMaxY(imageViewChat!.frame)
+            var startX = padding
+            let startY:CGFloat = 5.0
+            if let _ = imageViewChat {
+                startX += CGRectGetMaxX(imageViewChat!.frame)
             }
-            labelChatText = UILabel(frame: CGRectMake(startX, startY, CGRectGetWidth(self.frame) - 2 * startX , 5))
+            
+            if data.type == .Mine {
+                labelChatText = UILabel(frame: CGRectMake(startX, startY, CGRectGetWidth(self.frame) - 2 * startX , 5))
+            } else {
+                labelChatText = UILabel(frame: CGRectMake(-5, startY, CGRectGetWidth(self.frame) - 2 * startX + 10 , 5))
+            }
             labelChatText?.textAlignment = data.type == .Mine ? .Right : .Left
-            labelChatText?.font = UIFont.systemFontOfSize(14)
+            labelChatText?.font = UIFont.systemFontOfSize(12)
             labelChatText?.numberOfLines = 0 // Making it multiline
             labelChatText?.text = data.text
             labelChatText?.sizeToFit() // Getting fullsize of it
-            self.addSubview(labelChatText!)
+            if data.type == .Mine {
+            } else {
+            }
+            
+            var _startY:CGFloat = 0.0
+            if let _ = labelChatText {
+                _startY += CGRectGetMaxY(labelChatText!.frame)
+            }
+            
+            if data.type == .Mine {
+                self.addSubview(labelChatText!)
+                timeAgoLbl = UILabel(frame: CGRectMake(startX, _startY, CGRectGetWidth(self.frame) - 2 * startX , 10))
+                self.addSubview(timeAgoLbl!)
+                subView.hidden = true
+            } else {
+                subView.addSubview(labelChatText!)
+                timeAgoLbl = UILabel(frame: CGRectMake(-5, _startY, CGRectGetWidth(self.frame) - 2 * startX, 10))
+                subView.addSubview(timeAgoLbl!)
+            }
+            
+            timeAgoLbl?.textAlignment = .Left
+            timeAgoLbl?.font = UIFont.systemFontOfSize(10)
+            timeAgoLbl?.text = data.date?.timeAgo
         }
         
         // 4. Calculation of new width and height of the chat bubble view
@@ -66,11 +95,11 @@ class ChatBubble: UIView {
         var viewWidth: CGFloat = 0.0
         if let imageView = imageViewChat {
             // Height calculation of the parent view depending upon the image view and text label
-            viewWidth = max(CGRectGetMaxX(imageViewChat!.frame), CGRectGetMaxX(labelChatText!.frame)) + padding
-            viewHeight = max(CGRectGetMaxY(imageViewChat!.frame), CGRectGetMaxY(labelChatText!.frame)) + padding
+            viewWidth = max(CGRectGetMaxX(labelChatText!.frame), CGRectGetMaxX(timeAgoLbl!.frame)) + padding
+            viewHeight = max(CGRectGetMaxY(labelChatText!.frame), CGRectGetMaxY(timeAgoLbl!.frame)) + padding
             
         } else {
-            viewHeight = CGRectGetMaxY(labelChatText!.frame) + padding/2
+            viewHeight = CGRectGetMaxY(timeAgoLbl!.frame) + padding/2
             viewWidth = CGRectGetWidth(labelChatText!.frame) + CGRectGetMinX(labelChatText!.frame) + padding
         }
         
@@ -80,22 +109,41 @@ class ChatBubble: UIView {
         // 6. Adding the resizable image view to give it bubble like shape
         let bubbleImageFileName = data.type == .Mine ? "bubbleMine" : "bubbleSomeone"
         imageViewBG = UIImageView(frame: CGRectMake(0.0, 0.0, CGRectGetWidth(self.frame), CGRectGetHeight(self.frame)))
+        let vChat = UIView(frame: CGRectMake(0.0, 0.0, CGRectGetWidth(self.frame), CGRectGetHeight(self.frame)))
+        
         if data.type == .Mine {
             imageViewBG?.image = UIImage(named: bubbleImageFileName)?.resizableImageWithCapInsets(UIEdgeInsetsMake(14, 14, 17, 28))
+            vChat.layer.backgroundColor = ImageUtil.UIColorFromRGB(0xdcf8c6).CGColor  //UIColor.yellowColor().CGColor
         } else {
             imageViewBG?.image = UIImage(named: bubbleImageFileName)?.resizableImageWithCapInsets(UIEdgeInsetsMake(14, 22, 17, 20))
+            vChat.layer.backgroundColor = ImageUtil.UIColorFromRGB(0xEFEFF4).CGColor
         }
-        self.addSubview(imageViewBG!)
-        self.sendSubviewToBack(imageViewBG!)
+        self.addSubview(vChat)
+        self.sendSubviewToBack(vChat)
+        //self.addSubview(imageViewBG!)
+        //self.sendSubviewToBack(imageViewBG!)
         
         // Frame recalculation for filling up the bubble with background bubble image
-        let repsotionXFactor:CGFloat = data.type == .Mine ? 0.0 : -8.0
+        let repsotionXFactor:CGFloat = data.type == .Mine ? 0.0 : -15.0
         let bgImageNewX = CGRectGetMinX(imageViewBG!.frame) + repsotionXFactor
         let bgImageNewWidth =  CGRectGetWidth(imageViewBG!.frame) + CGFloat(12.0)
         let bgImageNewHeight =  CGRectGetHeight(imageViewBG!.frame) + CGFloat(6.0)
-        imageViewBG?.frame = CGRectMake(bgImageNewX, 0.0, bgImageNewWidth, bgImageNewHeight)
-
+        imageViewBG?.frame = CGRectMake(bgImageNewX - 5, -3.0, bgImageNewWidth + 5, bgImageNewHeight + 5)
+        vChat.frame = CGRectMake(bgImageNewX - 5, -3.0, bgImageNewWidth + 5, bgImageNewHeight )
+        vChat.bounds = CGRectInset(vChat.frame, 5.0, 3.0)
         
+        vChat.layer.cornerRadius = 5.0
+        vChat.layer.masksToBounds = true
+        
+        subView.frame = CGRectMake(20, -3.0, bgImageNewWidth + 5, bgImageNewHeight )
+        subView.bounds = CGRectInset(vChat.frame, 0.0, 0.0)
+        
+        subView.layer.cornerRadius = 5.0
+        subView.layer.masksToBounds = true
+        
+        //if data.type == .Mine {
+        //    subView.hidden = true
+        //}
         // Keepping a minimum distance from the edge of the screen
         var newStartX:CGFloat = 0.0
         if data.type == .Mine {
