ec4252296aa95fdd18a815e877ed36769e368595
Mindnervestech <admin@Macs-MacBook-Pro.local>
2016-03-30 16:48:59 +0530

    conversation change

--- babybox/babybox/ChatBubble.swift
+++ babybox/babybox/ChatBubble.swift
@@ -49,12 +49,13 @@ class ChatBubble: UIView {
         }
         
         let messageView: UIView = UIView(frame: CGRectMake(startX, 0, self.frame.width, self.frame.height))
-        ViewUtil.displayMessageView(messageView)
-        ViewUtil.displayMessageView(self)
         if (data.type == .Opponent) {
             messageView.backgroundColor = Color.WHITE
             messageView.layer.cornerRadius = 4
+            ViewUtil.displayMessageView(messageView)
             self.addSubview(messageView)
+        } else {
+            ViewUtil.displayMessageView(self)
         }
         
         // 2. Going to add Text if any
